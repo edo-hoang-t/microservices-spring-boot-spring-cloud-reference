46c62d5355f1e4768b49f95981d9e6250b4300a4
Hoang Tran <descience.thh10@gmail.com>
2022-10-11 01:46:55 +0700

    initial commit

---
+++ git-localconfig-repo/README.txt
@@ -0,0 +1,3 @@
+SETUP GUIDELINE:
+- Initialize a git folder here
+- Place it in the correct location specified in the config of .properties file of "spring-cloud-config-server"
---
+++ naming-server/src/test/java/namingserver/CurrencyConversionService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
